3ff1bc934b74abd8e8d2c76f5829402c65aee1b8
AndrewPerks <andrew-perkins@live.co.uk>
2016-10-20 22:51:32 +0100

    Capitalise and camel case to spine jasmine

--- JasmineTests/scripts/string-extensions.js
+++ JasmineTests/scripts/string-extensions.js
@@ -1,4 +1,23 @@
-﻿String.prototype.capitalise = function () {}
-String.prototype.camelCaseToSpineCase = function () { }
-String.prototype.spineCaseToCamelCase = function () { }
-String.prototype.format = function () { }
+﻿String.prototype.capitalise = function() {
+    var input = this;
+    var capitalisedString = input.replace(input, input.charAt(0).toUpperCase() + input.substr(1).toLowerCase());
+    return capitalisedString;
+}
+
+String.prototype.camelCaseToSpineCase = function() {
+    var input = this;
+   
+    var newInput = input.replace(/\s/g, "-").replace(/([A-Z])/g, "-$1").toLowerCase();
+    var inputNoRepeats = newInput.replace(/([-])\1*/g, "-"); // remove consecutive -
+    return inputNoRepeats;
+}
+
+String.prototype.spineCaseToCamelCase = function() {
+    var input = this;
+    return input;
+}
+
+String.prototype.format = function() {
+    var input = this;
+    return input;
+}
