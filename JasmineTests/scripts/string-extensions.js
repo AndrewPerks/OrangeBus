3b6a73b08133d26c6638a8288b415efe5b77cbc9
AndrewPerks <andrew-perkins@live.co.uk>
2016-10-22 18:28:52 +0100

    Spine to camel case

--- JasmineTests/scripts/string-extensions.js
+++ JasmineTests/scripts/string-extensions.js
@@ -14,7 +14,18 @@ String.prototype.camelCaseToSpineCase = function() {
 
 String.prototype.spineCaseToCamelCase = function() {
     var input = this;
-    return input;
+    var output = "";
+
+    var trimmedInput = input.trim();
+    var firstLetter = trimmedInput.substring(0, 1).toLowerCase();
+    var afterFirstLetter = trimmedInput.substring(1);
+
+    var camelCased = afterFirstLetter.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); }).replace(/ ([a-z])/g, function (g) { return g[1].toUpperCase(); });
+    var camelNoExtraDashesOrWhite = camelCased.replace(/[-]/g, "").replace(/\s/g, "");
+    
+    output = firstLetter + camelNoExtraDashesOrWhite;
+ 
+    return output;
 }
 
 String.prototype.format = function() {
